141a737db9f00271b140bea29dab7a7bd22d101f
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-10-05 12:58:26 +0530

    5 class + 6 class outputs

--- forest + Boosting/shot(GBM Boosting Classification model).R
+++ forest + Boosting/shot(GBM Boosting Classification model).R
@@ -1,5 +1,18 @@
-set.seed(200)
-data<- read.csv("./../shot 2.csv")
+arr=c("./leg.csv","./pull.csv","./scoop.csv","straight.csv","./cover.csv")
+data <- read.csv(arr[1])
+par(mar=c(1,1,1,1))
+for (i in arr[-1] )
+{
+  data2 <- read.csv(i,head=TRUE,sep=",");
+  data <- rbind(data,data2);
+}
+
+set.seed(189)
+data2 <- data[1:52]
+data2[data2 <0] <- NA
+data2[53]<- data[53]
+data2= na.omit(data2)
+data<- data2
 print("This would take 5 to 10 Minutes. Please Be patient")
 library(caTools)
 library(gbm)
@@ -7,7 +20,7 @@ split=sample.split(data,SplitRatio = 0.8)
 training <- subset(data,split ==T)
 testing <- subset(data,split ==F)
   boost.data = gbm (shotType~. , data=training,distribution = "multinomial",
-                    n.trees=7000,shrinkage=0.01,interaction.depth=4)
+                    n.trees=7000,shrinkage=0.01,interaction.depth=6,n.cores=5)
 
 print(summary(boost.data))
 
@@ -15,5 +28,5 @@ pred = predict.gbm(boost.data, newdata = testing, n.trees = 7000,type="response"
     dim(pred)
 library(caret)
 labels = colnames(pred)[apply(pred, 1, which.max)]
-print(confusionMatrix(testing$shotType,as.factor(labels)))
 
+print(confusionMatrix(testing$shotType,as.factor(labels)))
