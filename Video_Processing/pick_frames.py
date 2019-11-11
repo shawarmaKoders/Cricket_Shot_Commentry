b1a4b27fae3509227733921f04f6aff37eccd94b
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-11-11 13:29:50 +0530

    picking filename from terminal (vid_proc/frames_picker)

--- Video_Processing/pick_frames.py
+++ Video_Processing/pick_frames.py
@@ -2,11 +2,19 @@ from pandas import DataFrame, concat
 from numpy import rot90
 import os
 import cv2
+import sys
 
-shotType = 'straight'
-for video_number in range(1, 2):
+if __name__ == "__main__":
     print('------------------------------------------')
     print('------------------------------------------')
+    try:
+        shotType = sys.argv[1]
+    except:
+        print('Expected shot directory')
+    try:
+        video_number = int(sys.argv[2])
+    except:
+        print("Expected a video number")
     filename = f'{shotType}{video_number}.mp4'
     video_file = os.path.join('clips', shotType, filename)
     print(video_file)
@@ -63,7 +71,12 @@ for video_number in range(1, 2):
 
     print('Total Frames Processed:', total_frames_processed)
     print(df)
-    df.to_csv('fname.csv', index=False, header=False)
+    output_dir = 'image_frames_csvs'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    out_csv_name = f'{shotType}_{video_number}.csv'
+    write_file_path = os.path.join(output_dir, out_csv_name)
+    df.to_csv(write_file_path, index=False, header=False)
 
     video.release()
     cv2.destroyAllWindows()
